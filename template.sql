8f0efa32f9c3ffac06f9a48f7ee1cc675f8ea1c2
Richard Miller <richard@netpro.work>
2015-08-25 01:05:34 +0100

    Added template SQL

---
+++ template.sql
@@ -0,0 +1,8 @@
+INSERT INTO `tmp`(`ID`, `name`, `location`, `provider`, `score`) VALUES (NULL, 'Competitor 1', 'Scotland', 'New College Lanarkshire', '78');
+INSERT INTO `tmp`(`ID`, `name`, `location`, `provider`, `score`) VALUES (NULL, 'Competitor 2', 'Wales', 'Cardif College', '80');
+INSERT INTO `tmp`(`ID`, `name`, `location`, `provider`, `score`) VALUES (NULL, 'Competitor 3', 'England', 'Oxford Uni', '50');
+INSERT INTO `tmp`(`ID`, `name`, `location`, `provider`, `score`) VALUES (NULL, 'Competitor 4', 'Northern Ireland', 'Belfast MET', '25');
+INSERT INTO `tmp`(`ID`, `name`, `location`, `provider`, `score`) VALUES (NULL, 'Competitor 5', 'Scotland', 'City of Glasgow College', '54');
+INSERT INTO `tmp`(`ID`, `name`, `location`, `provider`, `score`) VALUES (NULL, 'Competitor 6', 'England', 'Birmingham Uni', '65');
+INSERT INTO `tmp`(`ID`, `name`, `location`, `provider`, `score`) VALUES (NULL, 'Competitor 7', 'England', 'London Uni', '45');
+INSERT INTO `tmp`(`ID`, `name`, `location`, `provider`, `score`) VALUES (NULL, 'Competitor 8', 'Wales', 'Coleg Sir Gar', '90');
